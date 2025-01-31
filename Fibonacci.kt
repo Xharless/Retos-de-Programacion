c66eab06c285069696b856df1eeb72800d313fb6
Xharless <carlosarevalo1702@hotmail.com>
2025-01-31 11:08:03 -0300

    ejercicios subidos

---
+++ Fibonacci.kt
@@ -0,0 +1,15 @@
+fun fibonacci(number: Int){
+    var n0: Long = 0
+    var n1: Long = 1
+    (1..number).forEach{ _ ->
+        println(n0)
+        var fib = n0 + n1
+        n0 = n1
+        n1 = fib
+    }
+
+}
+
+fun main(){
+    fibonacci(50)
+}
