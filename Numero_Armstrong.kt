89bb8ffd2f0a16dd7206161ef8936f763b057770
Xharless <carlosarevalo1702@hotmail.com>
2025-02-14 09:45:07 -0300

    numero de armstrong

--- Numero_Armstrong.kt
+++ Numero_Armstrong.kt
@@ -1,18 +1,29 @@
+import kotlin.math.*
+import java.lang.Math
 
-fun Armstrong(number: Int): Boolean {
-    if(number<0){
-        return false
-    }else{
-        for(i in number){
-            prinln(i)
+
+fun Armstrong(number: Int) {
+    if(number < 0){
+        println("No es numero de Armstrong")
+    } else {
+        var num = number.toString()
+        var largo = num.length
+        var suma = 0.0
+        for(i in num){
+            var result = i.digitToInt()
+            suma += Math.pow(result.toDouble(), largo.toDouble())
+        }
+        if(suma.toInt() == number){
+            println("Es numero de Armstrong")
+        } else {
+            println("No es numero de Armstrong")
         }
     }
 }
 
 fun main(){
-    if(Armstrong(153)){
-
-    } else{
-        println("No es numero de Armstrong")
-    }
+    Armstrong(371)
+    Armstrong(-371)
+    Armstrong(372)
+    Armstrong(0)
 }
